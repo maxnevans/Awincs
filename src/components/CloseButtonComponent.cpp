df420cfa7ce5c858702182dbd5d99bfac1727846
Maxim Shcherbakov <maxnevans@gmail.com>
2020-04-18 19:28:45 +0300

    CloseButtonComponent features implemented

--- src/components/CloseButtonComponent.cpp
+++ src/components/CloseButtonComponent.cpp
@@ -6,6 +6,22 @@
 
 namespace Awincs
 {
+	CloseButtonComponent::CloseButtonComponent()
+	{
+		p_setBackgroundColor(DEFAULT_BACKGROUND_COLOR, ComponentState::DEFAULT);
+		p_setBackgroundColor(DEFAULT_HOVER_BACKGROUND_COLOR, ComponentState::HOVER);
+		p_setBackgroundColor(DEFAULT_ACTIVE_BACKGROUND_COLOR, ComponentState::ACTIVE);
+		
+		p_setTextColor(DEFAULT_TEXT_COLOR, ComponentState::DEFAULT);
+		p_setTextColor(DEFAULT_HOVER_TEXT_COLOR, ComponentState::HOVER);
+		p_setTextColor(DEFAULT_ACTIVE_TEXT_COLOR, ComponentState::ACTIVE);
+		
+		p_setText(L"x");
+		p_setFontSize(16);
+		p_setFontFamily(L"Lucida Console");
+		p_setBold();
+	}
+
 	CloseButtonComponent::ShouldParentHandleEvent CloseButtonComponent::handleEvent(const Event::Mouse::ButtonEvent& e)
 	{
 		auto shouldHandle = ButtonComponent::handleEvent(e);
--- src/components/CloseButtonComponent.h
+++ src/components/CloseButtonComponent.h
@@ -11,6 +11,15 @@ namespace Awincs
 		public ButtonComponent
 	{
 	public:
+		CloseButtonComponent();
 		virtual ShouldParentHandleEvent handleEvent(const Event::Mouse::ButtonEvent&) override;
+
+	protected:
+		static constexpr gp::ARGB DEFAULT_BACKGROUND_COLOR = makeARGB(0x00, 0x00, 0x00, 0x00);
+		static constexpr gp::ARGB DEFAULT_HOVER_BACKGROUND_COLOR = makeARGB(0x00, 0x00, 0x00, 0x00);
+		static constexpr gp::ARGB DEFAULT_ACTIVE_BACKGROUND_COLOR = makeARGB(0xff, 207, 81, 43);
+		static constexpr gp::ARGB DEFAULT_TEXT_COLOR = makeARGB(0xff, 224, 224, 224);
+		static constexpr gp::ARGB DEFAULT_HOVER_TEXT_COLOR = makeARGB(0xff, 207, 81, 43);
+		static constexpr gp::ARGB DEFAULT_ACTIVE_TEXT_COLOR = makeARGB(0xff, 224, 224, 224);
 	};
 }
