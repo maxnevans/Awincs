8716b7df0ad45ede394c0d09779770bbc3fd2d64
Maxim Shcherbakov <maxnevans@gmail.com>
2020-04-22 13:24:51 +0300

    CloseButtonComponent cross drawn with lines not text

--- src/components/CloseButtonComponent.cpp
+++ src/components/CloseButtonComponent.cpp
@@ -11,17 +11,7 @@ namespace Awincs
         p_setBackgroundColor(DEFAULT_BACKGROUND_COLOR, ComponentState::DEFAULT);
         p_setBackgroundColor(DEFAULT_HOVER_BACKGROUND_COLOR, ComponentState::HOVER);
         p_setBackgroundColor(DEFAULT_ACTIVE_BACKGROUND_COLOR, ComponentState::ACTIVE);
-        
-        p_setTextColor(DEFAULT_TEXT_COLOR, ComponentState::DEFAULT);
-        p_setTextColor(DEFAULT_HOVER_TEXT_COLOR, ComponentState::HOVER);
-        p_setTextColor(DEFAULT_ACTIVE_TEXT_COLOR, ComponentState::ACTIVE);
-        
-        p_setText(L"x");
-        p_setFontSize(16);
-        p_setFontFamily(L"Lucida Console");
-        p_setBold();
     }
-
     CloseButtonComponent::ShouldParentHandleEvent CloseButtonComponent::handleEvent(const Event::Mouse::ButtonEvent& e)
     {
         auto shouldHandle = ButtonComponent::handleEvent(e);
@@ -35,5 +25,38 @@ namespace Awincs
         }
 
         return true;
+    }
+    void CloseButtonComponent::draw(gp::Graphics& gfx) const
+    {
+        ButtonComponent::draw(gfx);
+
+        gp::Pen pen{ gp::Color{crossColors.at(p_getState())}, DEFAULT_CROSS_WIDTH };
+
+        auto transformedCrossVertices = p_transformVertices(std::vector<FltPoint>(&DEFAULT_CROSS_VERTICES[0], &DEFAULT_CROSS_VERTICES[4]));
+        auto replaceCallback = [this](const FltPoint& pt) -> gp::Point { 
+            auto p = p_transformToGlobal(pt);
+            return {static_cast<INT>(p.x), static_cast<INT>(p.y)};
+        };
+        auto prepared = std::vector<gp::Point>{};
+        prepared.reserve(transformedCrossVertices.capacity());
+        std::transform(transformedCrossVertices.begin(), transformedCrossVertices.end(), std::back_inserter(prepared), replaceCallback);
+
+        gfx.DrawLine(&pen, prepared[0], prepared[1]);
+        gfx.DrawLine(&pen, prepared[2], prepared[3]);
+    }
+    std::vector<CloseButtonComponent::FltPoint> CloseButtonComponent::p_transformVertices(std::vector<FltPoint> vtcs) const
+    {
+        auto [width, height] = p_getDimensions();
+        const float wFactor = width / 2.f;
+        const float hFactor = height / 2.f;
+        const float sFactor = DEFAULT_CROSS_SIZE;
+
+        for (auto& vtx : vtcs)
+        {
+            vtx.x = vtx.x * sFactor * wFactor + wFactor;
+            vtx.y = vtx.y * sFactor * hFactor + hFactor;
+        }
+
+        return vtcs;
     }
 }
--- src/components/CloseButtonComponent.h
+++ src/components/CloseButtonComponent.h
@@ -11,15 +11,32 @@ namespace Awincs
         public ButtonComponent
     {
     public:
+        using FltPoint = Geometry::Point2D<float>;
+    public:
         CloseButtonComponent();
+
+        /* Event handlers */
         virtual ShouldParentHandleEvent handleEvent(const Event::Mouse::ButtonEvent&) override;
 
     protected:
-        static constexpr gp::ARGB DEFAULT_BACKGROUND_COLOR                      = makeARGB(0x00, 0x00, 0x00, 0x00);
-        static constexpr gp::ARGB DEFAULT_HOVER_BACKGROUND_COLOR                = makeARGB(0x00, 0x00, 0x00, 0x00);
-        static constexpr gp::ARGB DEFAULT_ACTIVE_BACKGROUND_COLOR               = makeARGB(0xff, 207, 81, 43);
-        static constexpr gp::ARGB DEFAULT_TEXT_COLOR                            = makeARGB(0xff, 224, 224, 224);
-        static constexpr gp::ARGB DEFAULT_HOVER_TEXT_COLOR                      = makeARGB(0xff, 207, 81, 43);
-        static constexpr gp::ARGB DEFAULT_ACTIVE_TEXT_COLOR                     = makeARGB(0xff, 224, 224, 224);
+        virtual void draw(gp::Graphics&) const override;
+
+        std::vector<FltPoint> p_transformVertices(std::vector<FltPoint>) const;
+
+    private:
+        static constexpr gp::ARGB DEFAULT_BACKGROUND_COLOR                          = makeARGB(0x00, 0x00, 0x00, 0x00);
+        static constexpr gp::ARGB DEFAULT_HOVER_BACKGROUND_COLOR                    = makeARGB(0x00, 0x00, 0x00, 0x00);
+        static constexpr gp::ARGB DEFAULT_ACTIVE_BACKGROUND_COLOR                   = makeARGB(0xff, 207, 81, 43);
+        static constexpr gp::ARGB DEFAULT_CROSS_COLOR                               = makeARGB(0xff, 224, 224, 224);
+        static constexpr gp::ARGB DEFAULT_HOVER_CROSS_COLOR                         = makeARGB(0xff, 207, 81, 43);
+        static constexpr gp::ARGB DEFAULT_ACTIVE_CROSS_COLOR                        = makeARGB(0xff, 224, 224, 224);
+        static constexpr gp::REAL DEFAULT_CROSS_WIDTH                               = 2.f;
+        static constexpr FltPoint DEFAULT_CROSS_VERTICES[]                          = { {-1.0f,  1.0f}, { 1.0f, -1.0f},   // top left to bottom right
+                                                                                        { 1.0f,  1.0f}, {-1.0f, -1.0f} }; // top right to bottom left
+        static constexpr float DEFAULT_CROSS_SIZE                                   = .5f;
+
+        std::map<ComponentState, gp::ARGB> crossColors                              = { {ComponentState::DEFAULT, DEFAULT_CROSS_COLOR},
+                                                                                        {ComponentState::HOVER,   DEFAULT_HOVER_CROSS_COLOR},
+                                                                                        {ComponentState::ACTIVE,  DEFAULT_ACTIVE_CROSS_COLOR} };
     };
 }
