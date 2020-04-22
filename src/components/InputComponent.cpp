f35a50a5504289b862b0e4b77c811ca108cded81
Maxim Shcherbakov <maxnevans@gmail.com>
2020-04-22 12:24:21 +0300

    InputComponent style control implemented

--- src/components/InputComponent.cpp
+++ src/components/InputComponent.cpp
@@ -8,6 +8,56 @@ namespace Awincs
         onTextChangeCallback = cb;
     }
 
+    void InputComponent::setBackgroundColor(gp::ARGB color, ComponentState state)
+    {
+        p_setBackgroundColor(color, state);
+    }
+
+    void InputComponent::setTextColor(gp::ARGB color, ComponentState state)
+    {
+        p_setTextColor(color, state);
+    }
+
+    void InputComponent::setCursorColor(gp::ARGB color, ComponentState state)
+    {
+        p_setCursorColor(color, state);
+    }
+
+    void InputComponent::setOutlineColor(gp::ARGB color, ComponentState state)
+    {
+        p_setOutlineColor(color, state);
+    }
+
+    void InputComponent::setOutlineWidth(int px)
+    {
+        p_setOutlineWidth(px);
+    }
+
+    void InputComponent::setFontFamily(std::wstring fontFamily)
+    {
+        p_setFontFamily(fontFamily);
+    }
+
+    void InputComponent::setFontSize(int size)
+    {
+        p_setFontSize(size);
+    }
+
+    void InputComponent::setFontUnit(gp::Unit unit)
+    {
+        p_setFontUnit(unit);
+    }
+
+    void InputComponent::setBold(bool enabled)
+    {
+        p_setBold(enabled);
+    }
+
+    void InputComponent::setItalic(bool enabled)
+    {
+        p_setItalic(enabled);
+    }
+
     const std::wstring& InputComponent::getText() const
     {
         return text;
@@ -58,16 +108,75 @@ namespace Awincs
         auto [width, height] = p_getDimensions();
         auto [x, y] = p_transformToGlobal(Point{0,0});
 
-        gfx.FillRectangle(&gp::SolidBrush{ gp::Color{DEFAULT_BACKGROUND_COLOR} }, gp::Rect{ x, y, width, height });
+        gfx.FillRectangle(&gp::SolidBrush{ gp::Color{backgroundColors.at(p_getState())} }, gp::Rect{ x, y, width, height });
 
-        if (p_isFocused())
+        if (outlineWidth > 0 && p_isFocused())
         {
-            gfx.DrawRectangle(&gp::Pen{ gp::Color{DEFAULT_OUTLINE_COLOR} }, gp::Rect{ x, y, width, height });
+            gfx.DrawRectangle(&gp::Pen{ gp::Color{outlineColors.at(p_getState())}, outlineWidth }, gp::Rect{ x, y, width, height });
         }
 
         gfx.DrawString(text.c_str(), static_cast<INT>(text.size()),
-            &gp::Font{ DEFAULT_TEXT_FONT_FAMILY, DEFAULT_TEXT_SIZE },
+            &gp::Font{ fontFamily.c_str(), fontSize, fontStyle, fontUnit},
             gp::PointF{ static_cast<gp::REAL>(x), static_cast<gp::REAL>(y) },
-            &gp::SolidBrush(gp::Color{ DEFAULT_TEXT_COLOR }));
+            &gp::SolidBrush(gp::Color{ textColors.at(p_getState()) }));
+    }
+    void InputComponent::p_setBackgroundColor(gp::ARGB color, ComponentState state)
+    {
+        backgroundColors[state] = color;
+    }
+    void InputComponent::p_setTextColor(gp::ARGB color, ComponentState state)
+    {
+        textColors[state] = color;
+    }
+    void InputComponent::p_setCursorColor(gp::ARGB color, ComponentState state)
+    {
+        cursorColors[state] = color;
+    }
+    void InputComponent::p_setOutlineColor(gp::ARGB color, ComponentState state)
+    {
+        outlineColors[state] = color;
+    }
+    void InputComponent::p_setOutlineWidth(int px)
+    {
+        outlineWidth = static_cast<gp::REAL>(px);
+    }
+    void InputComponent::p_setFontFamily(std::wstring fontFamily)
+    {
+        this->fontFamily = fontFamily;
+    }
+    void InputComponent::p_setFontSize(int size)
+    {
+        fontSize = static_cast<gp::REAL>(size);
+    }
+    void InputComponent::p_setFontUnit(gp::Unit unit)
+    {
+        fontUnit = unit;
+    }
+    void InputComponent::p_setBold(bool enabled)
+    {
+        isBold = enabled;
+
+        if (isItalic)
+        {
+            fontStyle = enabled ? gp::FontStyle::FontStyleBoldItalic : gp::FontStyle::FontStyleItalic;
+        }
+        else
+        {
+            fontStyle = enabled ? gp::FontStyle::FontStyleBold : gp::FontStyle::FontStyleRegular;
+        }
+    }
+
+    void InputComponent::p_setItalic(bool enabled)
+    {
+        isItalic = enabled;
+
+        if (isBold)
+        {
+            fontStyle = enabled ? gp::FontStyle::FontStyleBoldItalic : gp::FontStyle::FontStyleBold;
+        }
+        else
+        {
+            fontStyle = enabled ? gp::FontStyle::FontStyleItalic : gp::FontStyle::FontStyleRegular;
+        }
     }
 }
--- src/components/InputComponent.h
+++ src/components/InputComponent.h
@@ -17,20 +17,73 @@ namespace Awincs
         virtual const std::wstring& getText() const;
         virtual void setText(std::wstring text);
         virtual void onTextChange(OnInputEvent cb);
+        virtual void setBackgroundColor(gp::ARGB color, ComponentState state = ComponentState::DEFAULT);
+        virtual void setTextColor(gp::ARGB color, ComponentState state = ComponentState::DEFAULT);
+        virtual void setCursorColor(gp::ARGB color, ComponentState state = ComponentState::DEFAULT);
+        virtual void setOutlineColor(gp::ARGB color, ComponentState state = ComponentState::DEFAULT);
+        virtual void setOutlineWidth(int px);
+        virtual void setFontFamily(std::wstring fontFamily);
+        virtual void setFontSize(int size);
+        virtual void setFontUnit(gp::Unit unit);
+        virtual void setBold(bool enabled = true);
+        virtual void setItalic(bool enabled = true);
+
+        /* Event handlers */
         virtual ShouldParentHandleEvent handleEvent(const Event::Keyboard::InputEvent&) override;
         virtual ShouldParentHandleEvent handleEvent(const Event::Mouse::ButtonEvent& e) override;
 
     protected:
         virtual void draw(gp::Graphics&) const override;
+        void p_setBackgroundColor(gp::ARGB color, ComponentState state = ComponentState::DEFAULT);
+        void p_setTextColor(gp::ARGB color, ComponentState state = ComponentState::DEFAULT);
+        void p_setCursorColor(gp::ARGB color, ComponentState state = ComponentState::DEFAULT);
+        void p_setOutlineColor(gp::ARGB color, ComponentState state = ComponentState::DEFAULT);
+        void p_setOutlineWidth(int px);
+        void p_setFontFamily(std::wstring fontFamily);
+        void p_setFontSize(int size);
+        void p_setFontUnit(gp::Unit unit);
+        void p_setBold(bool enabled = true);
+        void p_setItalic(bool enabled = true);
 
     private:
-        static constexpr gp::ARGB DEFAULT_BACKGROUND_COLOR              = makeARGB(25, 50, 100);
-        static constexpr gp::ARGB DEFAULT_CURSOR_COLOR                  = makeARGB(255, 255, 255);
-        static constexpr gp::ARGB DEFAULT_TEXT_COLOR                    = makeARGB(170, 170, 170);
         static constexpr const wchar_t* DEFAULT_TEXT_FONT_FAMILY        = L"Tahoma";
-        static constexpr gp::REAL DEFAULT_TEXT_SIZE                     = 17;
         static constexpr gp::ARGB DEFAULT_OUTLINE_COLOR                 = makeARGB(45, 122, 186);
+        static constexpr gp::ARGB DEFAULT_HOVER_OUTLINE_COLOR           = makeARGB(45, 122, 186);
+        static constexpr gp::ARGB DEFAULT_ACTIVE_OUTLINE_COLOR          = makeARGB(45, 122, 186);
+        static constexpr gp::ARGB DEFAULT_BACKGROUND_COLOR              = makeARGB(25, 50, 100);
+        static constexpr gp::ARGB DEFAULT_HOVER_BACKGROUND_COLOR        = makeARGB(45, 122, 186);
+        static constexpr gp::ARGB DEFAULT_ACTIVE_BACKGROUND_COLOR       = makeARGB(45, 122, 186);
+        static constexpr gp::ARGB DEFAULT_CURSOR_COLOR                  = makeARGB(255, 255, 255);
+        static constexpr gp::ARGB DEFAULT_HOVER_CURSOR_COLOR            = makeARGB(255, 255, 255);
+        static constexpr gp::ARGB DEFAULT_ACTIVE_CURSOR_COLOR           = makeARGB(255, 255, 255);
+        static constexpr gp::ARGB DEFAULT_TEXT_COLOR                    = makeARGB(170, 170, 170);
+        static constexpr gp::ARGB DEFAULT_HOVER_TEXT_COLOR              = makeARGB(170, 170, 170);
+        static constexpr gp::ARGB DEFAULT_ACTIVE_TEXT_COLOR             = makeARGB(170, 170, 170);
+        static constexpr const gp::REAL DEFAULT_OUTLINE_WIDTH           = 1.f;
+        static constexpr const gp::REAL DEFAULT_FONT_SIZE               = 14;
+        static constexpr const gp::Unit DEFAULT_FONT_UNIT               = gp::Unit::UnitPixel;
+        static constexpr const wchar_t* DEFAULT_FONT_FAMILY             = L"Tahoma";
+        static constexpr const gp::FontStyle DEFAULT_FONT_STYLE         = gp::FontStyle::FontStyleRegular;
 
+        std::map<ComponentState, gp::ARGB> outlineColors                = { {ComponentState::DEFAULT,   DEFAULT_OUTLINE_COLOR},
+                                                                            {ComponentState::HOVER,     DEFAULT_HOVER_OUTLINE_COLOR}, 
+                                                                            {ComponentState::ACTIVE,    DEFAULT_ACTIVE_OUTLINE_COLOR} };
+        std::map<ComponentState, gp::ARGB> backgroundColors             = { {ComponentState::DEFAULT,   DEFAULT_BACKGROUND_COLOR},
+                                                                            {ComponentState::HOVER,     DEFAULT_HOVER_BACKGROUND_COLOR},
+                                                                            {ComponentState::ACTIVE,    DEFAULT_ACTIVE_BACKGROUND_COLOR} };
+        std::map<ComponentState, gp::ARGB> cursorColors                 = { {ComponentState::DEFAULT,   DEFAULT_CURSOR_COLOR},
+                                                                            {ComponentState::HOVER,     DEFAULT_HOVER_CURSOR_COLOR},
+                                                                            {ComponentState::ACTIVE,    DEFAULT_ACTIVE_CURSOR_COLOR} };
+        std::map<ComponentState, gp::ARGB> textColors                   = { {ComponentState::DEFAULT,   DEFAULT_TEXT_COLOR},
+                                                                            {ComponentState::HOVER,     DEFAULT_HOVER_TEXT_COLOR},
+                                                                            {ComponentState::ACTIVE,    DEFAULT_ACTIVE_TEXT_COLOR} };
+        gp::REAL outlineWidth                                           = DEFAULT_OUTLINE_WIDTH;
+        gp::Unit fontUnit                                               = DEFAULT_FONT_UNIT;
+        gp::REAL fontSize                                               = DEFAULT_FONT_SIZE;
+        std::wstring fontFamily                                         = DEFAULT_FONT_FAMILY;
+        gp::FontStyle fontStyle                                         = DEFAULT_FONT_STYLE;
+        bool isBold                                                     = false;
+        bool isItalic                                                   = false;
         std::wstring text = L"";
         OnInputEvent onTextChangeCallback;
     };
